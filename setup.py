e732fffd5148056f66019370657154b23d58592c
stuzeneger <60490497+stuzeneger@users.noreply.github.com>
2020-12-17 03:04:36 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'LVPersonCodeUtils',
+    version = '0.0.1',
+    description = 'Bibliotēka Latvijas personu koda pārbaudei',
+    author = 'Einars Maslinovskis',
+    packages = ['lvpkutils'],
+    author_email = 'stuzeneger@rambler.ru'
+    )
